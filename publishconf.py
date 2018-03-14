efa01e900290930052be05f96d3c84ded11a3636
michaeloneill <michael.oneill86@gmail.com>
2018-03-14 18:48:38 +0000

    try again

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://michaeloneill.github.io'
+SITEURL = 'https://michaeloneill.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
